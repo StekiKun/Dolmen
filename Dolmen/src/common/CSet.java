c4f39f4c6c55091267fa37d79d31c0748afda5f3
StekiKun <steki.kun@gmail.com>
2017-04-04 00:33:14 +0200

    Implement classic operations on character sets (union, intersection, difference, and complement)

--- Dolmen/src/common/CSet.java
+++ Dolmen/src/common/CSet.java
@@ -11,6 +11,9 @@ import org.eclipse.jdt.annotation.Nullable;
  * character intervals, with some special values for degenerate
  * cases, and are thus optimized for space rather than for lookup speed.
  * 
+ * The special value 0xFFFF is reserved to denote end of file.
+ * It is not a valid Unicode character anyway.
+ * 
  * Instances of this class are immutable.
  * 
  * @author Stéphane Lescuyer
@@ -110,6 +113,11 @@ public abstract class CSet {
 	}
 	
 	/**
+	 * Special character value used to denote end-of-file
+	 */
+	public static final char EOF = 0xFFFF;
+	
+	/**
 	 * Describes a single contiguous interval of characters,
 	 * from {@link #first} to {@link #last}, inclusive.
 	 * 
@@ -163,15 +171,15 @@ public abstract class CSet {
 	 * @author Stéphane Lescuyer
 	 */
 	private static final class Intervals extends CSet {
-		final Interval head;
+		@Nullable final Interval head;
 
-		private Intervals(Interval head) {
+		private Intervals(@Nullable Interval head) {
 			this.head = head;
 		}
 		
 		@Override
 		public boolean isEmpty() {
-			return false;	// head is non-null
+			return head != null;
 		}
 
 		@Override
@@ -189,7 +197,8 @@ public abstract class CSet {
 		public String toString() {
 			StringBuilder buf = new StringBuilder();
 			buf.append("[");
-			head.append(buf);
+			if (head != null)
+				head.append(buf);
 			buf.append("]");
 			@SuppressWarnings("null")
 			@NonNull String res = buf.toString();
@@ -207,5 +216,177 @@ public abstract class CSet {
 	public static CSet interval(char first, char last) {
 		if (first > last) throw new IllegalArgumentException();
 		return new Intervals(new Interval(first, last, null));
+	}
+	
+	private static CSet intervals(@Nullable Interval i) {
+		if (i == null) return EMPTY;
+		if (i.next != null) return new Intervals(i);
+		if (i.first == i.last) return new Singleton(i.first);
+		if (i.first == 0 && i.last == 0xFFFF) return ALL;
+		return new Intervals(i);
+	}
+	
+	/*
+	 * Character set operations
+	 */
+	
+	private static final Interval ALL_INTERVAL =
+		new Interval((char)0, (char)0xFFFF, null);
+	
+	private static @Nullable Interval intervalsOf(CSet cs) {
+		if (cs instanceof Intervals)
+			return ((Intervals) cs).head;
+		if (cs instanceof Singleton) {
+			char c = ((Singleton) cs).c;
+			return new Interval(c, c, null);
+		}
+		if (cs == ALL) return ALL_INTERVAL;
+		if (cs == EMPTY) return null;
+		throw new IllegalStateException();
+	}
+	
+	/**
+	 * @param i1
+	 * @param i2
+	 * @return the union of two ordered lists of intervals
+	 * 		as an ordered list of intervals
+	 */
+	private static 
+	@Nullable Interval iunion(
+		@Nullable Interval i1, @Nullable Interval i2) {
+		if (i1 == null) return i2;
+		if (i2 == null) return i1;
+		if (i1.first > i2.first) return iunion(i2, i1);
+		if (i1.last + 1 < i2.first) {
+			return new Interval(i1.first, i1.last,
+								iunion(i1.next, i2));
+		} else if (i1.last < i2.last) {
+			return iunion(
+					new Interval(i1.first, i2.last, i2.next),
+					i1.next);
+		} else {
+			return iunion(i1, i2.next);
+		}
+	}
+	
+	/**
+	 * Returns the union of the two given character sets
+	 */
+	public static CSet union(CSet cs1, CSet cs2) {
+		if (cs1 == EMPTY || cs2 == ALL) return cs2;
+		if (cs2 == EMPTY || cs1 == ALL) return cs1;
+		// No EMPTY, no ALL
+		return intervals(
+				iunion(intervalsOf(cs1), intervalsOf(cs2)));		
+	}
+	
+	private static char maxChar(char c1, char c2) {
+		if (c1 > c2) return c1;
+		return c2;
+	}
+	
+	/**
+	 * @param i1
+	 * @param i2
+	 * @return the intersection of two ordered list
+	 * 		of intervals, as an ordered list of intervals
+	 */
+	private static 
+	@Nullable Interval iinter(
+		@Nullable Interval i1, @Nullable Interval i2) {
+		if (i1 == null || i2 == null) return null;
+		if (i1.last < i2.first) {
+			return iinter(i1.next, i2);
+		} else if (i2.last < i1.first){
+			return iinter(i1, i2.next);
+		} else if (i2.last < i2.last) {
+			return new Interval(maxChar(i1.first, i2.first), 
+						i1.last,
+						iinter(i1.next, i2));
+		} else {
+			return new Interval(maxChar(i1.first, i2.first),
+						i2.last,
+						iinter(i1, i2.next));
+		}
+	}
+	
+	/**
+	 * Returns the intersection of the two given character sets
+	 */
+	public static CSet inter(CSet cs1, CSet cs2) {
+		if (cs1 == EMPTY || cs2 == EMPTY) return EMPTY;
+		if (cs1 == ALL) return cs2;
+		if (cs2 == ALL) return cs1;
+		if (cs1 instanceof Singleton) {
+			char c1 = ((Singleton) cs1).c;
+			if (cs2.contains(c1)) return cs1;
+			else return EMPTY;
+		}
+		if (cs2 instanceof Singleton) {
+			char c2 = ((Singleton) cs2).c;
+			if (cs1.contains(c2)) return cs2;
+		}
+		return intervals(
+				iinter(intervalsOf(cs1), intervalsOf(cs2)));
+	}
+	
+	/**
+	 * @param i1
+	 * @param i2
+	 * @return the difference between the ordered intervals
+	 * 		{@code i1} and {@code i2}, in this order
+	 */
+	private static
+	@Nullable Interval idiff(
+		@Nullable Interval i1, @Nullable Interval i2) {
+		if (i2 == null) return i1;
+		if (i1 == null) return null;
+		if (i1.last < i2.first) {
+			return new Interval(i1.first, i1.last,
+					idiff(i1.next, i2));
+		} else if (i2.last < i1.first){
+			return idiff(i1, i2.next);
+		} else {
+			Interval r = i1.next;
+			if (i2.last < i2.first)
+				r = new Interval((char) (i2.last + 1), 
+							i2.first, r);
+			if (i1.first < i2.first) {
+				return new Interval(i1.first, (char) (i1.last - 1),
+							idiff(r, i2.next));
+			} else {
+				return idiff(r, i2.next);
+			}
+		}
+	}
+	
+	/**
+	 * @param cs1
+	 * @param cs2
+	 * @return the character set of all characters
+	 * 		in {@code cs1} except those that are in {@code cs2}
+	 */
+	public static CSet diff(CSet cs1, CSet cs2) {
+		if (cs1 == EMPTY) return EMPTY;
+		if (cs2 == EMPTY) return cs1;
+		if (cs1 instanceof Singleton) {
+			char c1 = ((Singleton) cs1).c;
+			if (cs2.contains(c1)) return EMPTY;
+			else return cs1;
+		}
+		if (cs2 instanceof Singleton) {
+			char c2 = ((Singleton) cs2).c;
+			if (!cs1.contains(c2)) return cs1;
+		}
+		return intervals(
+				idiff(intervalsOf(cs1), intervalsOf(cs2)));
+	}
+	
+	/**
+	 * @param cs
+	 * @return the complement of the character set {@code cs}
+	 */
+	public static CSet complement(CSet cs) {
+		return diff(ALL, cs);
 	}
 }
